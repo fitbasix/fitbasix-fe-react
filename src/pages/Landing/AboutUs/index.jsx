4636ef5a8315a10511fad2cba09c53aae3d4f358
MadhurVerma1893 <madhur.v@antino.io>
2023-05-19 17:23:54 +0530

    Who we are UI

--- src/pages/Landing/AboutUs/index.jsx
+++ src/pages/Landing/AboutUs/index.jsx
@@ -1,9 +1,111 @@
-import React from 'react'
-import "./styles.css"
+import { Button, Grid, Typography } from "@mui/material";
+import React from "react";
+import ReactPlayer from "react-player";
+import image1 from "../../../assets/cardImg1.svg";
+import image2 from "../../../assets/cardImg2.svg";
+import image3 from "../../../assets/cardImg3.svg";
+import "./styles.css";
 const AboutUs = () => {
   return (
-    <div className="aboutUsContainer">AboutUs</div>
-  )
-}
+    <Grid container spacing={2} className="aboutUsContainer">
+      <Grid item xs={6}>
+        <Grid container>
+          <Grid item xs={12}>
+            <Typography variant="h3">
+              <span style={{ color: "#49AD50", fontWeight: 700 }}>Who </span>are
+              we?
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography>
+              Lorem ipsum dolor sit amet. Qui enim vitae eos quia inventore aut
+              deserunt quia et ducimus voluptatem. Quo natus voluptatem et
+              dignissimos molestiae eos ipsam repellendus ex totam rerum?
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <ReactPlayer
+              url="https://www.youtube.com/watch?v=es4x5R-rV9s"
+              light={true}
+              wrapper={"div"}
+              width={400}
+              height={300}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={6}>
+        <Grid container >
+          <Grid item>
+            <div className="cardContainer">
+              <div className="leftContainer">
+                <img src={image1} />
+              </div>
+              <div className="rightContainer">
+                <div className="cardTitle">
+                  <Typography variant="h6">Nutritional Plan</Typography>
+                </div>
+                <div>
+                  <Typography>
+                    Lorem ipsum dolor sit amet. Qui enim vitae eos quia
+                    inventore aut deserunt quia et d
+                  </Typography>
+                </div>
 
-export default AboutUs
+                <div className="btnTrial">
+                  <Button variant="contained">Get a FREE TRIAL!</Button>
+                </div>
+              </div>
+            </div>
+          </Grid>
+          <Grid item>
+            <div className="cardContainer">
+              <div className="leftContainer">
+                <img src={image2} />
+              </div>
+              <div className="rightContainer">
+                <div className="cardTitle">
+                  <Typography variant="h6">Nutritional Plan</Typography>
+                </div>
+                <div>
+                  <Typography>
+                    Lorem ipsum dolor sit amet. Qui enim vitae eos quia
+                    inventore aut deserunt quia et d
+                  </Typography>
+                </div>
+
+                <div className="btnTrial">
+                  <Button variant="contained">Get a FREE TRIAL!</Button>
+                </div>
+              </div>
+            </div>
+          </Grid>
+          <Grid item>
+            <div className="cardContainer">
+              <div className="leftContainer">
+                <img src={image3} />
+              </div>
+              <div className="rightContainer">
+                <div className="cardTitle">
+                  <Typography variant="h6">Nutritional Plan</Typography>
+                </div>
+                <div>
+                  <Typography>
+                    Lorem ipsum dolor sit amet. Qui enim vitae eos quia
+                    inventore aut deserunt quia et d
+                  </Typography>
+                </div>
+
+                <div className="btnTrial">
+                  <Button variant="contained">Get a FREE TRIAL!</Button>
+                </div>
+              </div>
+            </div>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default AboutUs;
--- src/pages/Landing/BMI/index.jsx
+++ src/pages/Landing/BMI/index.jsx
@@ -1,9 +1,59 @@
-import React from 'react'
-import "./styles.css"
+import React from "react";
+import "./styles.css";
+import { Grid, TextField, Typography } from "@mui/material";
+import { Button, Input } from "@mui/material";
 const BMI = () => {
   return (
-    <div className='BMIContainer'>BMI</div>
-  )
-}
+    <div className="BMIContainer">
+      <Grid container spacing={5} className="gridContainer">
+        <Grid item xs={12}>
+          <Typography variant="h3">KNOW YOUR BMI</Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <TextField
+            fullWidth
+            placeholder="Gender"
+            id="gender"
+            className="textField"
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <TextField
+            fullWidth
+            placeholder="Weight in kgs"
+            id="weight"
+            className="textField"
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <TextField
+            fullWidth
+            placeholder="Height in cms"
+            id="height"
+            className="textField"
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <Button
+            variant="contained"
+            fullWidth
+            className="calcButton"
+            sx={{
+              backgroundColor: "#0D1F2D",
+              borderRadius: "10px",
+              textTransform: "none",
+           
+            }}
+          >
+            Calculate
+          </Button>
+        </Grid>
+        <Grid item xs={2}>
+          <Typography variant="h4">BMI</Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
-export default BMI
+export default BMI;
